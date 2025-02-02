9c60f2e9e9cab2aeb1b5fe1f6be5403dd461d4a0
XPaladiumyX <allegra.yann@gmail.com>
2025-02-02 18:32:18 +0100

    first commit

---
+++ src/main/java/skyxnetwork/skyXMysteryBox/SkyXMysteryBox.java
@@ -0,0 +1,17 @@
+package skyxnetwork.skyXMysteryBox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyXMysteryBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
